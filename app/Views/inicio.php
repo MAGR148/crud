91dedc1cfce80ba6062f870b8f61b354e74572ff
miguel Gómez <migr14820@gmail.com>
2022-06-22 18:16:16 -0500

    Agregando idUsuario al formulario de agregar

--- app/Views/inicio.php
+++ app/Views/inicio.php
@@ -24,10 +24,11 @@
     </div>
     </nav>
     <div class="container">
-      <h1>CRUD Peliculas</h1>
+      <h1>Mundo pelis</h1>
       <div class="row">
         <div class="col-sm-12">
           <form method="POST" action="<?php echo base_url().'/crear'?>">
+            <input type="hidden" name="idUsuario" value="<?php echo session('idUsuario'); ?>">
             <div class="form-floating">
               <input type="text" name="titulo" placeholder="Titulo de la película" id="titulo" class="form-control">
               <label for="titulo">Titulo de la película</label>
@@ -59,7 +60,26 @@
           </form>
         </div>
       </div>
+      <hr>
+      <h2>Listado de películas</h2>
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="table table-responsive">
+            <table class="table table-hover table-bordered">
+              <tr>
+                <th>Título</th>
+                <th>Año</th>
+                <th>Idioma</th>
+                <th>Clasificación</th>
+                <th>Sinopsis</th>
+              </tr>
+
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
   </body>
 </html>
