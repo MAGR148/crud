20596bc9ceda0b1cacad61f88e64fba0d0f00b16
miguel Gómez <migr14820@gmail.com>
2022-06-22 15:57:58 -0500

    Formulario para ingresar datos

--- app/Views/inicio.php
+++ app/Views/inicio.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Inicio</title>
+    <title>Listado de Películas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   </head>
   <body>
@@ -23,7 +23,42 @@
         </div>
     </div>
     </nav>
-    <h1>Estas dentro del inicio</h1>
+    <div class="container">
+      <h1>CRUD Peliculas</h1>
+      <div class="row">
+        <div class="col-sm-12">
+          <form method="POST" action="<?php echo base_url().'/crear'?>">
+            <div class="form-floating">
+              <input type="text" name="titulo" placeholder="Titulo de la película" id="titulo" class="form-control">
+              <label for="titulo">Titulo de la película</label>
+            </div><br>
+            <div class="form-floating">
+              <input type="number" name="anio" placeholder="Año de lanzamiento" id="anio" class="form-control">
+              <label for="anio">Año de lanzamiento</label>
+            </div><br>
+            <div class="form-floating">
+              <input type="text" name="idioma" placeholder="Idioma" id="idioma" class="form-control">
+              <label for="idioma">Idioma original de la película</label>
+            </div><br>
+            <div class="form-floating">
+              <select class="form-select" name="clasificacion" id="clasificacion" aria-label="Default select example">
+                <option selected disabled>Elige una opción</option>
+                <option value="AA">AA - Apto para todo público infantil</option>
+                <option value="A">A - Apto para todo público</option>
+                <option value="B">B - 12 años en adelante</option>
+                <option value="C">C - 18 años en adelante</option>
+                <option value="D">D - Solo para adultos</option>
+              </select>
+              <label for="clasificacion">Clasificación</label>
+            </div><br>
+            <div class="form-floating">
+              <textarea class="form-control" placeholder="Sinopsis de la película" id="sinopsis" style="height: 100px"></textarea>
+              <label for="sinopsis">Sinopsis</label>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
   </body>
 </html>
