6665645fff098d0fc749455cec14100ce0f8abcc
miguel Gómez <migr14820@gmail.com>
2022-06-23 01:34:05 -0500

    Generando querys para la tabal de usuarios

--- app/Models/usuarios.php
+++ app/Models/usuarios.php
@@ -8,4 +8,19 @@
             $usuario->where($data);
             return $usuario->get()->getResultArray(); 
         }
+        //Método para insertar un nuevo usuario
+        public function insertar($data){
+            $usuario = $this->db->table('t_usuario');
+            $usuario->insert($data);
+
+            return $this->db->insertID();
+        }
+
+        public function contarUsuario($usuario) {
+            $builder= $this->db->table('t_usuario');
+            $count = $builder->select('usuario')
+                            ->where('usuario', $usuario)
+                            ->countAllResults();
+            return $count;
+        }
     }
