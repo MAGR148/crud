14ed9cb4966645b626eab31c67174783bb0b3248
miguel Gómez <migr14820@gmail.com>
2022-06-23 12:12:52 -0500

    Dando estilos al formulario de registro

--- app/Views/formRegistro.php
+++ app/Views/formRegistro.php
@@ -7,6 +7,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   </head>
   <body>
+    <nav class="navbar navbar-expand-lg" style="background-color: #8F4CF5 ;">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="<?php echo base_url().'/inicio'?>" style="color:white;">
+          <img src="<?php echo base_url('/images/palomitas.png')?>" alt="" width="60" height="48" class="d-inline-block align-text-top"> Mundo Pelis
+        </a>
+      </div>
+    </nav><br>
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
@@ -14,31 +21,42 @@
                 <form action="<?php echo base_url('/registrar') ?>" method="POST">
                 <input type="hidden" name="type" value="admin">
                   <div class="form-floating">
-                    <input type="text" name="nombre" placeholder="Nombre(s)" id="nombre" class="form-control">
+                    <input type="text" name="nombre" placeholder="Nombre(s)" id="nombre" class="form-control" required>
                     <label for="nombre">Nombre(s)</label>
                   </div><br>
                   <div class="form-floating">
-                    <input type="text" name="a_paterno" placeholder="Apellido paterno" id="a_paterno" class="form-control">
+                    <input type="text" name="a_paterno" placeholder="Apellido paterno" id="a_paterno" class="form-control" required>
                     <label for="a_paterno">Apellido paterno</label>
                   </div><br>
                   <div class="form-floating">
-                    <input type="text" name="a_materno" placeholder="Apellido materno" id="a_materno" class="form-control">
+                    <input type="text" name="a_materno" placeholder="Apellido materno" id="a_materno" class="form-control" required>
                     <label for="a_materno">Apellido materno</label>
                   </div><br>
                   <div class="form-floating">
-                    <input type="email" name="usuario" placeholder="Nombre de usuario" id="usuario" class="form-control">
+                    <input type="email" name="usuario" placeholder="Nombre de usuario" id="usuario" class="form-control" required>
                     <label for="usuario">Correo de usuario</label>
                   </div><br>
                   <div class="form-floating">
-                    <input type="password" name="password" placeholder="Contraseña" id="password" class="form-control">
+                    <input type="password" name="password" placeholder="Contraseña" id="password" class="form-control" required>
                     <label for="password">Contraseña</label>
                   </div><br>
                   <div class="form-floating">
-                    <input type="password" name="password1" placeholder="Confirmación de Contraseña" id="password1" class="form-control">
+                    <input type="password" name="password1" placeholder="Confirmación de Contraseña" id="password1" class="form-control" required>
                     <label for="password1">Confirmación de contraseña</label>
                   </div><br>
-                    <a href="<?php echo base_url().'/' ?>" class="btn btn-danger">Volver</a>
-                    <button class="btn btn-success">Registrarme</button>
+                  <div class="col-12">
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+                      <label class="form-check-label" for="invalidCheck">
+                        Estoy de acuerdo en registrarme
+                      </label>
+                      <div class="invalid-feedback">
+                        Debes estar de acuerdo antes de registrarte.
+                      </div>
+                    </div>
+                  </div><br>
+                  <a href="<?php echo base_url().'/' ?>" class="btn btn-danger ">Volver</a>
+                  <button class="btn btn-success " >Registrarme</button>
                 </form>
             </div>
             <div class="col-sm-4"></div>
