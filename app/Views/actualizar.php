43edd24e70e883fb8b7847f702e97afba09bf83d
miguel Gómez <migr14820@gmail.com>
2022-06-23 12:12:35 -0500

    dando estilos a la vista de actualizar

--- app/Views/actualizar.php
+++ app/Views/actualizar.php
@@ -17,21 +17,24 @@
   </head>
   <body>
 
-    <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="<?php echo base_url().'/inicio'?>"><?php echo session('nombre'); ?></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?php echo base_url('/salir') ?>">Salir</a>
-            </li>
-        </ul>
-        </div>
-    </div>
-    </nav>
+    <nav class="navbar navbar-expand-lg" style="background-color: #8F4CF5 ;">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="<?php echo base_url().'/inicio'?>">
+          <img src="<?php echo base_url('/images/palomitas.png')?>" alt="" width="60" height="48" class="d-inline-block align-text-top">
+        </a>
+          <a class="navbar-brand" style="color: white;" href="<?php echo base_url().'/inicio'?>">Bienvenido <?php echo session('nombre'); ?></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="position: absolute;left: 95%; transform: translatex(-50%)">
+                <li class="nav-item">
+                  <button class="btn btn-danger"><a class="nav-link active" style="color: white;" aria-current="page" href="<?php echo base_url('/salir') ?>">Salir</a></button>
+                </li>
+            </ul>
+          </div>
+      </div>
+    </nav><br>
     <div class="container">
       <h1>Actualizar Peliculas</h1>
       <div class="row">
